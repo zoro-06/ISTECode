620bd2b25d5298d57fbefca007cc3828e46d06ef
Shivam Tewari <shivamtewari2002@gmail.cm>
2024-02-17 18:33:33 +0530

    viewport_2

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,34 +3,34 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { useSetRecoilState } from "recoil";
-
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-    const setAuthModalState = useSetRecoilState(authModalState);
+	const setAuthModalState = useSetRecoilState(authModalState);
+	const handleClick = () => {
+		setAuthModalState((prev) => ({ ...prev, isOpen: true }));
+	};
+	return (
+		<div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
+			<Link href='/' className='flex items-center justify-center h-20'>
+			<img src="1iste.png" alt="Description of the GIF"  width={70} height={70}/>
+				<Image src='/3iste.png' alt='ISTECode' height={200} width={200} />
+			</Link>
+			<div className='flex items-center'>
+				
+				
 
-    const handleClick = () => {
-        setAuthModalState((prev) => ({ ...prev, isOpen: true }));
-    };
-
-    return (
-        <div className='flex flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between px-4 md:px-24'>
-            <Link href='/' className='flex items-center justify-center'>
-                <img src="/1iste.png" alt="Description of the GIF" width={70} height={70} />
-                <Image src='/3iste.png' alt='ISTECode' height={200} width={200} />
-            </Link>
-            <div className='flex items-center mt-4 sm:mt-0'>
-                <button
-                    className='bg-brand-purple text-white px-4 py-2 rounded-md text-sm font-medium
-                        hover:text-brand-purple hover:bg-white hover:border-brand-purple hover:border-3 
-                        border-transparent transition duration-300 ease-in-out'
-                    onClick={handleClick}
-                >
-                    Log In
-                </button>
-            </div>
-        </div>
-    );
+				<button
+					className='bg-brand-purple text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                hover:text-brand-purple hover:bg-white hover:border-3 hover:border-brand-purple border-3 border-transparent
+                transition duration-300 ease-in-out marhin-left:20px
+                '
+					onClick={handleClick}
+				>
+					Log In
+				</button>
+			</div>
+		</div>
+	);
 };
-
 export default Navbar;

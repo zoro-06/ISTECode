4d028d384b1ebd12712ad70c3ae454a3ce5d4881
Shivam Tewari <shivamtewari2002@gmail.cm>
2024-02-17 18:13:02 +0530

    viewport_1

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react";
 
 export default function useWindowSize() {
-	const [windowSize, setWindowSize] = useState({
-		width: typeof window !== "undefined" ? window.innerWidth : 1200,
-		height: typeof window !== "undefined" ? window.innerHeight : 800,
-	});
+  const [windowSize, setWindowSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 375, // Default to iPhone 6/7/8 width
+    height: typeof window !== "undefined" ? window.innerHeight : 667, // Default to iPhone 6/7/8 height
+  });
 
-	function changeWindowSize() {
-		if (typeof window !== "undefined") {
-			setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-		}
-	}
+  function changeWindowSize() {
+    if (typeof window !== "undefined") {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+  }
 
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			window.addEventListener("resize", changeWindowSize);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        requestAnimationFrame(changeWindowSize);
+      };
 
-			return () => {
-				window.removeEventListener("resize", changeWindowSize);
-			};
-		}
-	}, []);
+      window.addEventListener("resize", handleResize);
 
-	return windowSize;
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
+  return windowSize;
 }

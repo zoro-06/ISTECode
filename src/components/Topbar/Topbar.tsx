f09e28cfbad38baea3a9a386925e36d1a80defe6
Shivam Tewari <shivamtewari2002@gmail.cm>
2024-02-18 00:12:57 +0530

    viewport_8

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -12,89 +12,127 @@ import Timer from "../Timer/Timer";
 import { useRouter } from "next/router";
 import { problems } from "@/utils/problems";
 import { Problem } from "@/utils/types/problem";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+
 
 type TopbarProps = {
-    problemPage?: boolean;
+	problemPage?: boolean;
 };
 
 const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
-    const [user] = useAuthState(auth);
-    const setAuthModalState = useSetRecoilState(authModalState);
-    const router = useRouter();
+	const [user] = useAuthState(auth);
+	const setAuthModalState = useSetRecoilState(authModalState);
+	const router = useRouter();
 
-    const handleProblemChange = (isForward: boolean) => {
-        const { order } = problems[router.query.pid as string] as Problem;
-        const direction = isForward ? 1 : -1;
-        const nextProblemOrder = order + direction;
-        const nextProblemKey = Object.keys(problems).find((key) => problems[key].order === nextProblemOrder);
+	const handleProblemChange = (isForward: boolean) => {
+		const { order } = problems[router.query.pid as string] as Problem;
+		const direction = isForward ? 1 : -1;
+		const nextProblemOrder = order + direction;
+		const nextProblemKey = Object.keys(problems).find((key) => problems[key].order === nextProblemOrder);
 
-        if (isForward && !nextProblemKey) {
-            const firstProblemKey = Object.keys(problems).find((key) => problems[key].order === 1);
-            router.push(`/problems/${firstProblemKey}`);
-        } else if (!isForward && !nextProblemKey) {
-            const lastProblemKey = Object.keys(problems).find(
-                (key) => problems[key].order === Object.keys(problems).length
-            );
-            router.push(`/problems/${lastProblemKey}`);
-        } else {
-            router.push(`/problems/${nextProblemKey}`);
-        }
-    };
+		if (isForward && !nextProblemKey) {
+			const firstProblemKey = Object.keys(problems).find((key) => problems[key].order === 1);
+			router.push(`/problems/${firstProblemKey}`);
+		} else if (!isForward && !nextProblemKey) {
+			const lastProblemKey = Object.keys(problems).find(
+				(key) => problems[key].order === Object.keys(problems).length
+			);
+			router.push(`/problems/${lastProblemKey}`);
+		} else {
+			router.push(`/problems/${nextProblemKey}`);
+		}
+	};
 
-    return (
-        <nav className='relative flex h-[70px] w-full shrink-0 items-center px-5 bg-gradient-to-b from-purple-700 to-black  relative text-dark-purple-7'>
-            <div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""}`}>
-                <Link href='/' className='h-[25px] flex items-center'>
-                    <Image src='/1iste.png' alt='Logo' height={100} width={80} />
-                    <Image src='/3iste.png' alt='logo img' width={150} height={80} />
-                </Link>
+	return (
+		<nav className='relative flex h-[70px] w-full shrink-0 items-center px-5 bg-gradient-to-b from-purple-700 to-black  relative text-dark-purple-7'>
+			<div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""}`}>
+				<Link href='/' className='h-[25px] flex'>
+					<Image src='/1iste.png' alt='Logo' height={100} width={80}  />
+					<Image src='/3iste.png' alt='logo img' width={150} height={80}  style={{ marginRight: '300px' }} />
+				</Link>
 
-                {problemPage && (
-                    <div className='flex items-center gap-4 flex-1 justify-center'>
-                        <div className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer' onClick={() => handleProblemChange(false)}>
-                            <FaChevronLeft />
-                        </div>
-                        <Link href='/' className='flex items-center gap-2 font-medium text-dark-gray-8 cursor-pointer'>
-                            <div><BsList /></div>
-                            <p className='whitespace-nowrap overflow-hidden overflow-ellipsis'>Problem List</p>
-                        </Link>
-                        <div className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer' onClick={() => handleProblemChange(true)}>
-                            <FaChevronRight />
-                        </div>
-                    </div>
-                )}
+				{problemPage && (
+					<div className='flex items-center gap-4 flex-1 justify-center'>
+						<div
+							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
+							onClick={() => handleProblemChange(false)}
+						>
+							<FaChevronLeft />
+						</div>
+						<Link
+							href='/'
+							className='flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer'
+						>
+							<div>
+								<BsList />
+							</div>
+							<p>Problem List</p>
+						</Link>
+						<div
+							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
+							onClick={() => handleProblemChange(true)}
+						>
+							<FaChevronRight />
+						</div>
+					</div>
+				)}
 
-                <div className='flex items-center space-x-2 flex-1 justify-end'>
-                    <a href='https://echo-sync.vercel.app/' target='_blank' rel='noreferrer' className='bg-brand-purple text-brand-orange px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+
+				<div className='flex items-center space-x-4 flex-1 justify-end'>
+				
+					<div>
+						<a
+							href='https://echo-sync.vercel.app/'
+							target='_blank'
+							rel='noreferrer'
+							className='bg-brand-purple text-brand-orange px-2 py-1 sm:px-4 rounded-md text-sm font-medium
 							hover:text-brand-purple hover:bg-white hover:border-3 hover:border-brand-purple border-3 border-transparent
-							transition duration-300 ease-in-out'>MEET</a>
-                    {!user && (
-                        <Link href='/auth' onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}>
-                            <button className='bg-brand-purple text-brand-orange px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+							transition duration-300 ease-in-out'
+						>
+							MEET
+						</a>
+					</div>
+					{!user && (
+						<Link
+							href='/auth'
+							onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}
+						>
+							<button className='bg-brand-purple text-brand-orange px-2 py-1 sm:px-4 rounded-md text-sm font-medium
 							hover:text-brand-purple hover:bg-white hover:border-3 hover:border-brand-purple border-3 border-transparent
 							transition duration-300 ease-in-out'>Log In</button>
-                        </Link>
-                    )}
-                    {user && (
-                        <Link href='/questions/q' onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}>
-                            <button className='bg-brand-purple text-brand-orange px-2 py-1 sm:px-4 rounded-md text-sm font-medium
-							hover:text-brand-purple hover:bg-white hover:border-3 hover:border-brand-purple border-3 border-transparent
-							transition duration-300 ease-in-out'>Q.O.T.D</button>
-                        </Link>
-                    )}
-                    {user && problemPage && <Timer />}
-                    {user && (
-                        <div className='group relative'>
-                            <Image src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
-                            <div className='absolute top-10 left-2/4 -translate-x-2/4 mx-auto bg-dark-gray-7 text-black p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 transition-all duration-300'>
-                                <p className='text-sm'>{user.email}</p>
-                            </div>
-                        </div>
-                    )}
-                    {user && <Logout />}
-                </div>
-            </div>
-        </nav>
-    );
+						</Link>
+					)}
+					{user && (
+						
+						<Link 
+			
+						href={'/questions/q'}
+							onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}
+						>
+							<button className='bg-brand-purple text-brand-orange px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                hover:text-brand-purple hover:bg-white hover:border-3 hover:border-brand-purple border-3 border-transparent
+                transition duration-300 ease-in-out '>Q.O.T.D</button>
+						</Link>
+					)}
+				    
+					{user && problemPage && <Timer />}
+					
+					{user && (
+						<div className='cursor-pointer group relative'>
+							<Image src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
+							<div
+								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-gray-7 text-black p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out'
+							>
+								<p className='text-sm'>{user.email}</p>
+							</div>
+						</div>
+					)}
+					{user && <Logout />}
+				</div>
+			</div>
+		</nav>
+	);
 };
 export default Topbar;

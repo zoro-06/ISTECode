22086738394c7dae022d0029fa1b238bf8400344
Shivam Tewari <shivamtewari2002@gmail.cm>
2024-02-18 00:17:43 +0530

    viewport_8

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -8,36 +8,32 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 
-
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-	const authModal = useRecoilValue(authModalState);
-	const [user, loading, error] = useAuthState(auth);
-	const [pageLoading, setPageLoading] = useState(true);
-	const router = useRouter();
+    const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+    const [pageLoading, setPageLoading] = useState(true);
+    const router = useRouter();
 
-	useEffect(() => {
-		if (user) router.push("/");
-		if (!loading && !user) setPageLoading(false);
-	}, [user, router, loading]);
+    useEffect(() => {
+        if (user) router.push("/");
+        if (!loading && !user) setPageLoading(false);
+    }, [user, router, loading]);
 
-	if (pageLoading) return null;
-	
+    if (pageLoading) return null;
 
-	return (
-		<div className='bg-gradient-to-b from-purple-500 to-black h-screen relative'>
-			<div className='max-w-7xl mx-auto'>
-				<Navbar />
-				<div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
-					
-					<img src="1iste.png" alt="Description of the GIF"  width={400} height={400}/>
-					<Image src='/3iste.png' alt='logo img' width={200} height={200} />
-
-				</div>
-				{authModal.isOpen && <AuthModal />}
-			</div>
-		</div>
-	);
+    return (
+        <div className='bg-gradient-to-b from-purple-500 to-black min-h-screen'>
+            <Navbar />
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex flex-col items-center justify-center h-screen'>
+                    <img src="1iste.png" alt="Description of the GIF" width={300} height={300} className='mb-8' />
+                    <Image src='/3iste.png' alt='logo img' width={200} height={200} className='mb-8' />
+                </div>
+            </div>
+            {authModal.isOpen && <AuthModal />}
+        </div>
+    );
 };
 export default AuthPage;
